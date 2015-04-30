928b4f4f9c88c000817969e3164673a79a8bb6ff
jacopofontana <spillo17@gmail.com>
2015-04-30 16:01:45 +0200

    - populate item class

--- src/org/ssig/alpr/oop/what2buy/Item.java
+++ src/org/ssig/alpr/oop/what2buy/Item.java
@@ -1,7 +1,107 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 SGT1B SSIG.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+*/
+
 package org.ssig.alpr.oop.what2buy;
 
-/**
- * Created by jacopofontana on 30/04/15.
- */
+
 public class Item {
+
+
+    private String name;
+
+    private double price;
+
+    private int qty;
+
+    /**
+     * Costruttore della classe Item la quale rappresenta un singolo elemento della lista della spesa
+     *
+     * @param name
+     * @param price
+     * @param qty
+     */
+    public Item(String name, double price, int qty){
+        this.name = name;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    /**
+     * Ritorna il nome dell'articolo
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Imposta il nome dell'articolo
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Ritorna il prezzo di un dato articolo
+     * @return double
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * Imposta il prezzo di un dato articolo
+     * @param price
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    /**
+     * Ritorna la quantita di un dato articolo
+     * @return int qty
+     */
+    public int getQty() {
+        return qty;
+    }
+
+    /**
+     *  Imposta la quantita di un dato articolo
+     * @param qty
+     */
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    /**
+     *
+     * @return stringa contenente gli attributi dell'oggetto in forma leggibile
+     */
+    @Override
+    public String toString(){
+        return this.qty +" x "+this.name + " - " + this.qty*this.price;
+    }
 }
+
